67712a31a5778eda9a28afbdb002fd5b5174c116
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-07-13 04:14:11 -0600

    created a smple webpage using VS

---
+++ VSFinalAssignment/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// script.js
+const title = document.querySelector('h1');
+
+function animateTitle() {
+    title.style.color = 'blue';
+    setTimeout(() => {
+        title.style.color = 'red';
+    }, 1000);
+}
+
+animateTitle();
